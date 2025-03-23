a397f6418a388807daa75c823356ece6824d7b89
alex_man <alexmnsbs@gmail.com>
2025-03-23 11:59:37 +0300

    27. refactor

--- src/main/java/easy/p1__200/problem027/Solution27.java
+++ src/main/java/easy/p1__200/problem027/Solution27.java
@@ -5,20 +5,25 @@ import static org.junit.Assert.assertEquals;
 public class Solution27 {
 
     public static int removeElementTest(int[] nums, int val) {
-        return 4;
+        return removeElement(nums, val);
     }
 
     public static void main(String[] args) {
         int number = 4;
         int[] data = new int[]{0, 3, 1, 1, 2, 3, 3, 3};
+        int[] data02 = new int[]{0,1,2,2,3,0,4,2};
         int len = removeElement(data, 3);
         for (int i = 0; i < len; i++) {
             System.out.print(data[i] + (i == len - 1 ? "" : ", "));
         }
 
         assertEquals(number, len);
-        int lenTest = removeElementTest(data, 3);
+        assertEquals(removeElement(data02, 2), 5);
+        int[] dataTest = new int[]{0, 3, 1, 1, 2, 3, 3, 3};
+        int[] dataTest02 = new int[]{0,1,2,2,3,0,4,2};
+        int lenTest = removeElementTest(dataTest, 3);
         assertEquals(number, lenTest);
+        assertEquals(removeElementTest(dataTest02, 2), 5);
     }
 
     public static int removeElement(int[] nums, int val) {
