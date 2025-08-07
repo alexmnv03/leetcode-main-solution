50641c2b602b91a65779a6f5df2937521521209a
alex_man <alexmnsbs@gmail.com>
2025-08-07 12:25:06 +0300

    f.add 2

--- src/main/java/medium/p1__200/problem02/Solution2.java
+++ src/main/java/medium/p1__200/problem02/Solution2.java
@@ -1,10 +1,36 @@
 package medium.p1__200.problem02;
 
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
+import common.LinkedListUtils;
 import common.ListNode;
 
 public class Solution2 {
 
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+    private static ListNode l1;
+    private static ListNode l2;
+    private static ListNode expected;
+
+    public static ListNode addTwoNumbersTest(ListNode l1, ListNode l2) {
+        return addTwoNumbers(l1, l2);
+    }
+
+    public static void main(String[] args) {
+        //System.out.println(Arrays.toString(addTwoNumbers(nums, target)));
+        l1 = LinkedListUtils.contructLinkedList(new int[] {2, 4, 3});
+        l2 = LinkedListUtils.contructLinkedList(new int[] {5, 6, 4});
+        expected = LinkedListUtils.contructLinkedList(new int[] {7, 0, 8});
+        assertEquals(expected, addTwoNumbers(l1, l2));
+
+
+        l1 = LinkedListUtils.contructLinkedList(new int[] {2, 4, 3});
+        l2 = LinkedListUtils.contructLinkedList(new int[] {5, 6, 4});
+        expected = LinkedListUtils.contructLinkedList(new int[] {7, 0, 8});
+        assertEquals(expected, addTwoNumbersTest(l1, l2));
+    }
+
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         int carry = 0;
         ListNode pre = new ListNode(-1);
         ListNode tmp = pre;
